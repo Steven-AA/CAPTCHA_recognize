d7a7db44cd9c61aa0b55049773edce0ef0542fb7
Steven-AA <1692614071@qq.com>
2017-05-27 00:02:02 +0800

    - 重构 getCAPTCHA.py - 修改 README.md - 添加 .gitignore

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # 正方验证码识别（基于TensorFlow）
+<!-- TOC -->
+
+- [正方验证码识别（基于TensorFlow）](#正方验证码识别基于tensorflow)
+    - [验证码采集](#验证码采集)
+    - [TensorfFlow安装](#tensorfflow安装)
+
+<!-- /TOC -->
 ## 验证码采集
     pip install wget
-    python getCAPTHCA.py
+    python getCAPTHCA.py [num]
+num是指下载验证码的数量，不填的话默认3000
 ## TensorfFlow安装
 - 安装Python3.5.2
 - 安装CUDA 8.0
--- getCAPTCHA.py
+++ getCAPTCHA.py
@@ -1,6 +1,26 @@
+#coding:utf-8
 import wget
+import sys
+import os
+import time
 
-for i in range(3000):
+def get_captcha(num,url,path):
+    for i in range(num):
+        try:
+            wget.download(url, path + str(i) + str(time.time()*1000000) + '.jpg')
+            print('Successfully get CAPTCHA ' + str(i))
+        except:
+            print('Failed to get CAPTCHA')
+
+if __name__ == '__main__':
     url = 'http://jwxt.njupt.edu.cn/CheckCode.aspx'
-    wget.download(url, 'D://CAPTCHA/CAPTCHA_' + str(i) + '.jpg')
-    print('Successfully get CAPTCHA ' + str(i))
+    path = './CAPTCHA/'
+    try:
+        num = int(sys.argv[1])
+    except:
+        num = 3000
+    try:
+        os.mkdir('./CAPTCHA/')
+    except:
+        pass
+    get_captcha(num,url,path)
